a6f4db7c26896fca1a437dd201c08e03d2ccbfa7
catupto <yhxxm01@gmail.com>
2026-06-13 09:20:31 +0800

    Update Footer.vue

--- assets/Footer.vue
+++ assets/Footer.vue
@@ -14,7 +14,7 @@
     <div class="powered">Powered by <a href="https://workers.cloudflare.com/" target="_blank"
         rel="noopener noreferrer">Cloudflare Workers</a></div>
 
-    <div style="margin:10px 0;">© 2025 FlareDrive. All rights reserved.</div>
+    <div style="margin:10px 0;">© 2026 FlareDrive. All rights reserved.</div>
   </footer>
 </template>
 
